8c68ce09a00a5d9982f5a9dd8a2bdbba9988bd22
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-04-20 20:11:46 -0700

    Improved new size function. Now a range slider. Separate buttons for "Clear" and "New".

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const container = document.querySelector('#grid-box');
-
+const gridContainer = document.querySelector('#grid-container');
+const controlsContainer = document.querySelector('#controls-container')
 let squaresPerSide = 16;
-let item = [];
+let cell = [];
 let currentlyActive = false;
 
 createGrid(squaresPerSide);
@@ -9,49 +9,83 @@ createGrid(squaresPerSide);
 
 function createGrid(squaresPerSide) {
   clearGrid();
-  container.style.gridTemplateColumns = (`repeat(${squaresPerSide}, 1fr`);
-  container.style.gridTemplateRows = (`repeat(${squaresPerSide}, 1fr`);
+  gridContainer.style.gridTemplateColumns = (`repeat(${squaresPerSide}, 1fr`);
+  gridContainer.style.gridTemplateRows = (`repeat(${squaresPerSide}, 1fr`);
   let numberOfCells = squaresPerSide * squaresPerSide;
   for(let i = 0; i<numberOfCells; i++) {
-    item[i] = document.createElement('div');
-    item[i].classList.add('cell');
-    container.appendChild(item[i]);
+    cell[i] = document.createElement('div');
+    cell[i].classList.add('cell');
+    gridContainer.appendChild(cell[i]);
   }
 }
 function clearGrid() {
-  while(container.firstChild) {
-    container.removeChild(container.firstChild);
+  while(gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
   }
 }
 
 
-container.addEventListener('click', function() { togglePen()});
+gridContainer.addEventListener('click', function() { togglePen()});
 
 function togglePen() {
   if(!currentlyActive) {
-    item.forEach(square => {
-      square.addEventListener('mouseenter', activatePen);
+    cell.forEach(item => {
+      item.addEventListener('mouseenter', activatePen);
     })
     currentlyActive = true;
   } else {
-    item.forEach(square => {
-      square.removeEventListener('mouseenter', activatePen);
+    cell.forEach(item => {
+      item.removeEventListener('mouseenter', activatePen);
     })
     currentlyActive = false;
   }
 }
 function activatePen(e) {
-  console.log(e.target);
-  e.target.classList.add('drawn');
+  // e.target.classList.add('drawn');
+  e.target.style = 'background-color: blue';
+  // e.target.style = `background-color: ${randomColor()}`;
+}
+function randomColor() {
+  let red = Math.floor(Math.random()*200);
+  let green = Math.floor(Math.random()*255);
+  let blue = (Math.floor(Math.random()*155)+100);
+  let alpha = (0.5*Math.random()+0.5);
+  return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
 }
 
-const reset = document.querySelector('button');
-reset.addEventListener('click', function() {
-  item.forEach(square => {
-    square.classList.remove('drawn');
-  })
-  let squaresPerSide = prompt('How many squares per side would you like in the new grid?');
-  createGrid(squaresPerSide);
+const resetButton = document.querySelector('#clear');
+resetButton.addEventListener('click', clear);
+const newGridButton = document.querySelector('#new-grid');
+newGridButton.addEventListener('click', function() {
+  clear();
+  createGrid(newSize.value);
 })
+
+
+function clear() {
+  cell.forEach(item => {
+    item.style = 'background-color: white';
+  })
+  cell.forEach(item => {
+    item.removeEventListener('mouseenter', activatePen);
+  })
+  currentlyActive = false;
+}
+
+
+
+
+
+
+let newSize = document.querySelector('#new-size');
+newSize.value = 16;
+let displaySize = document.createElement('span');
+controlsContainer.appendChild(displaySize);
+displaySize.textContent = newSize.value;
+newSize.addEventListener('mousemove', function() {
+  console.log(newSize.value);
+  displaySize.textContent = newSize.value;
+})
+
 
 
