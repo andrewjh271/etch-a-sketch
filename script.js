c2621eb33de062a11cd69eeaf764cd7275d757a8
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-04-21 00:06:57 -0700

    Added full color functionality.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const controlsContainer = document.querySelector('#controls-container')
 let squaresPerSide = 16;
 let cell = [];
 let currentlyActive = false;
+let colorChoice = 0; //blue
+let currentColor = [];
 
 createGrid(squaresPerSide);
 
@@ -42,16 +44,81 @@ function togglePen() {
 }
 function activatePen(e) {
   // e.target.classList.add('drawn');
-  e.target.style = 'background-color: blue';
-  // e.target.style = `background-color: ${randomColor()}`;
+  colorChoice = +colorButtons.color.value; //converts to number
+  console.log(colorChoice);
+  switch(colorChoice) {
+    case(0):
+      currentColor = [19, 123, 214, 0.95];
+      e.target.style = `background-color: rgba(${currentColor})`;
+      break;
+    case(1):
+      currentColor = randomColor1();
+      e.target.style = `background-color: rgba(${currentColor})`; //emphasizes blue
+      break;
+    case(2):
+      currentColor = randomColor2();
+      e.target.style = `background-color: rgba(${currentColor})`; //emphasizes blue
+      break;
+    case(3):
+      currentColor = randomColor3();
+      e.target.style = `background-color: rgba(${currentColor})`; //emphasizes blue
+      break;
+    case(4):
+      currentColor = darken(e);
+      e.target.style = `background-color: rgba(${currentColor})`
+      break;
+    case(5):
+      currentColor = colorPicker.value;
+      e.target.style = `background-color: ${currentColor}`;
+      console.log(currentColor);
+      break;
+    default:
+      currentColor = [19, 123, 214, 0.95];
+      e.target.style = `background-color: rgba(${currentColor})`;
+      break;
+  }
 }
-function randomColor() {
-  let red = Math.floor(Math.random()*200);
-  let green = Math.floor(Math.random()*255);
-  let blue = (Math.floor(Math.random()*155)+100);
+function randomColor1() {
+  let red = Math.floor(Math.random()*150);
+  let green = Math.floor(Math.random()*220);
+  let blue = (Math.floor(Math.random()*135)+120);
   let alpha = (0.5*Math.random()+0.5);
-  return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+  return [red, green, blue, alpha];
+} //emphasizes blue
+function randomColor2() {
+  // let red = Math.floor(Math.random()*100+155);
+  let red = (Math.floor(Math.random()*135)+120);
+  let green = Math.floor(Math.random()*100);
+  let blue = Math.floor(Math.random()*255);
+  let alpha = (0.6*Math.random()+0.2);
+  return [red, green, blue, alpha];
+} //emphasizes red
+function randomColor3() {
+  let red = Math.floor(Math.random()*50);
+  let green = Math.floor(Math.random()*125+130);
+  let blue = (Math.floor(Math.random()*255));
+  let alpha = (0.5*Math.random()+0.5);
+  return [red, green, blue, alpha];
+} //emphasizes green
+function darken(e) {
+  console.log(e.target.style.backgroundColor);
+  let colorString = e.target.style.backgroundColor;
+  let red = (+(colorString.slice(colorString.indexOf('(')+1, colorString.indexOf(','))));
+  colorString = colorString.slice(colorString.indexOf(' ')+1);
+  let green = (+colorString.slice(0, colorString.indexOf(',')));
+  colorString = colorString.slice(colorString.indexOf(' ')+1);
+  let blue = (+colorString.slice(0, colorString.indexOf(',')));
+  colorString = colorString.slice(colorString.indexOf(' ')+1, colorString.indexOf(')'));
+  let alpha = +colorString;
+  console.log([red, green, blue, alpha]);
+  red = ((red - 25.5) < 0) ? 0 : red - 25.5;
+  green = ((green - 25.5) < 0) ? 0 : green - 25.5;
+  blue = ((blue - 25.5) < 0) ? 0 : blue - 25.5;
+  alpha = ((alpha + 0.1) >= 1) ? 1 : alpha + 0.99;
+  return [red, green, blue, alpha];
 }
+
+
 
 const resetButton = document.querySelector('#clear');
 resetButton.addEventListener('click', clear);
@@ -65,8 +132,9 @@ newGridButton.addEventListener('click', function() {
 function clear() {
   cell.forEach(item => {
     item.style = 'background-color: white';
-  })
-  cell.forEach(item => {
+    // item.classList.remove('drawn');
+    // item.classList.add('white');
+    displaySize.textContent = newSize.value;
     item.removeEventListener('mouseenter', activatePen);
   })
   currentlyActive = false;
@@ -75,17 +143,26 @@ function clear() {
 
 
 
+const colorButtons = document.querySelector('#radio-buttons');
+colorButtons.addEventListener('change', function(){
+  console.log(colorButtons.color.value)
+})
+
+const colorPicker = document.querySelector('#color-picker');
+colorPicker.addEventListener('change', function() {
+  console.log(colorPicker.value);
+})
+colorPicker.value = '#e490ff';
 
 
 let newSize = document.querySelector('#new-size');
 newSize.value = 16;
 let displaySize = document.createElement('span');
-controlsContainer.appendChild(displaySize);
+// controlsContainer.appendChild(displaySize);
+controlsContainer.insertBefore(displaySize, colorButtons);
 displaySize.textContent = newSize.value;
 newSize.addEventListener('mousemove', function() {
-  console.log(newSize.value);
+  // console.log(newSize.value);
   displaySize.textContent = newSize.value;
 })
-
-
 
